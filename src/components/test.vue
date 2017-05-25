eb5c87439e3b5a91d75046356bd5e66b43838917
Zach Cannon <zcannon@dmp.com>
2017-05-25 14:16:45 -0500

    Init commit

---
+++ src/components/test.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class='test'>
+    <input type='text' v-model='title'>
+    <br>
+    <h1>{{title}} </h1>
+    <p v-if='showName' v-text='user.first_name'></p>
+    <p v-else='showName'>Nobody</p> 
+    <ul>
+      <li v-for='item in items'>{{item.title}}</li>
+    </ul>
+    <button v-on:click='greet("Hello World")'>Say Greeting</button>
+    <br>
+    <input type='text' v-on:keyup='pressKey'>
+    
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'test',
+    data(){
+      return {
+        title: 'Hello World',
+        user: {
+          first_name: 'Zach',
+          last_name: 'Cannon'
+        },
+        showName: true,
+        items: [
+          {title: 'Item One'},          
+          {title: 'Item Two'},          
+          {title: 'Item Three'}
+        ]
+      }
+    },
+    methods: {
+      greet: function (greeting){
+        alert(greeting);
+      },
+      pressKey: function (e){
+        console.log(e.target.value)
+      }
+    }
+  }
+</script>
+<style scoped>
+
+</style>  
